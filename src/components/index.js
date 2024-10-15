bd3fba576c399f6d5122d5480373eac6bb2ae01b
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-10-15 19:17:00 +0530

    completing PostForm

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,14 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer"; 
 import Container from "./container/Container";
-import LogoutBtn from "./Header/LogoutBtn"; 
+import LogoutBtn from "./Header/LogoutBtn";  
+import RTE from "./RTE";
 
  
 export {
     Header,
     Footer,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    RTE
 }
